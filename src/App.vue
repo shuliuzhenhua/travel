72b8739f8f4e7430d1bb70d82042b4ba3709bdcf
testsha <test@qq.com>
2018-06-05 15:26:40 +0800

    '首页及城市选择页面功能完善'

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
-    <router-view/>
+    <!--把已加载的数据暂时存储在内存中，下次再加载时直接从内存中获取无需再重新加载数据-->
+    <keep-alive>
+      <router-view/>
+    </keep-alive>
   </div>
 </template>
 
